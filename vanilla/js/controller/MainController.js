e1bd039d392bc7000906484118157f8119599c45
danny <gryb809@gmail.com>
2021-06-29 17:22:43 +0900

    :sparkles: 탭 구현 및 기본 탭 설정 추가

--- vanilla/js/controller/MainController.js
+++ vanilla/js/controller/MainController.js
@@ -1,5 +1,6 @@
 import FormView from "../views/FormView.js"
 import ResultView from "../views/ResultView.js"
+import TabView from "../views/TabView.js"
 
 import SearchModel from "../models/SeachModel.js"
 
@@ -10,8 +11,20 @@ export default {
     FormView.setup(document.querySelector('form'))
       .on('@submit', e => this.onSubmit(e.detail.input))
       .on('@reset', e => this.onResetForm())
+      
+    TabView.setup(document.querySelector('#tabs'))
 
     ResultView.setup(document.querySelector('#search-result'))
+
+    this.selectedTab = '추천 검색어'
+    
+    this.renderView()
+  },
+
+  renderView() {
+    console.log(tag, 'renderView()')
+    TabView.setActiveTab(this.selectedTab)
+    ResultView.hide()
   },
 
   search(query) {
