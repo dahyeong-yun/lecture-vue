efa0717856ffa82c80f7ecc5688fd460d216a92f
danny <gryb809@gmail.com>
2021-06-30 14:48:22 +0900

    :sparkles: 검색 시, 최근 검색어 추가 기능 구현

--- vanilla/js/controller/MainController.js
+++ vanilla/js/controller/MainController.js
@@ -62,6 +62,7 @@ export default {
 
   search(query) {
     console.log(tag, 'search()', query)
+    HistoryModel.add(input)
     SearchModel.list(query).then(data => {
       this.onSearchResult(data)
     }) 
